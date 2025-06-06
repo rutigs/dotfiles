34441c11a3b042d8e0f6b71eb6ee7ce65cedef57
Nick Rutigliano <rutigs@users.noreply.github.com>
2025-06-05 18:33:20 -0700

    Update none-ls.lua

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,27 +6,26 @@ return {
         null_ls.setup({
             debug = true,
             sources = {
-                null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.goimports,
                 null_ls.builtins.formatting.clang_format,
             },
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({
-                        group = augroup,
-                        buffer = bufnr,
-                    })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = buf,
-                        callback = function()
-                            vim.lsp.buf.format({ bufnr = bufnr })
-                        end,
-                    })
-                end
+            -- on_attach = function(client, bufnr)
+            --     if client.supports_method("textDocument/formatting") then
+            --         vim.api.nvim_clear_autocmds({
+            --             group = augroup,
+            --             buffer = bufnr,
+            --         })
+            --         vim.api.nvim_create_autocmd("BufWritePre", {
+            --             group = augroup,
+            --             buffer = buf,
+            --             callback = function()
+            --                 vim.lsp.buf.format({ bufnr = bufnr })
+            --             end,
+            --         })
+            --     end
 
-            end,
+            -- end,
         })
 
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
